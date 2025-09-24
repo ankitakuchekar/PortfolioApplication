ae8f1c5091b8f1c91946d9e65647f4d3a2824be8
Ankita Kuchekar <kuchekarankita128@gmail.com>
2025-09-24 16:51:56 +0530

    commit for some build issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mynewapp"
+    namespace = "com.bold.portfolio"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
